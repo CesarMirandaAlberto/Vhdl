88d1f6f450a2fd5992a6a6822165f353a447e085
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-06 22:11:30 -0600

    Testbench de la entidad Demultiplexor 4 a 1

--- Basico/Demultiplexor4a1_TB.vhd
+++ Basico/Demultiplexor4a1_TB.vhd
@@ -3,6 +3,7 @@
 -- 	El presente código implementa el desarrollo del testbench para analizar 
 --  funcionamiento del multiplexor 1 a 4.
 --  DUT: Nombre de la instanciación a la entidad del demultiplexor
+--  la instanciación de la entidad es realizada por componente.
 -- ===========================================================================
 --Inclusión de librerias
 library IEEE;
@@ -26,38 +27,38 @@ architecture Testbench of Demultiplexor4a1_TB is
 		);
 	end component;
 --Señales locales del testbench	
-			signal X :  std_logic_vector(3 downto 0) := "1010";
-			signal Selector : std_logic_vector(3 downto 0) := "0000";
+			signal X_TB :  std_logic_vector(3 downto 0) := "1010";
+			signal Selector_TB : std_logic_vector(3 downto 0) := "0000";
 			
-			signal Salida1 :  std_logic_vector(3 downto 0);
-			signal Salida2 :  std_logic_vector(3 downto 0);
-			signal Salida3 :  std_logic_vector(3 downto 0);
-			signal Salida4 :  std_logic_vector(3 downto 0);
+			signal Salida1_TB :  std_logic_vector(3 downto 0);
+			signal Salida2_TB :  std_logic_vector(3 downto 0);
+			signal Salida3_TB :  std_logic_vector(3 downto 0);
+			signal Salida4_TB :  std_logic_vector(3 downto 0);
 	
 	begin
 		DUT: Demultiplexor4a1 --Instanciación de la entidad del demultiplexor
 			port map ( --Mapeo de señales
-				X => X,
-				Selector => selector,
-				Salida1 => Salida1,
-				Salida2 => Salida2,
-				Salida3 => Salida3,
-				Salida4 => Salida4
+				X => X_TB,
+				Selector => selector_TB,
+				Salida1 => Salida1_TB,
+				Salida2 => Salida2_TB,
+				Salida3 => Salida3_TB,
+				Salida4 => Salida4_TB
 			);
 			
 	Estimulos: process --Estimulos de las señales
 		begin
-				Selector <= "0000"; --Asignación de valores
+				Selector_TB <= "0000"; --Asignación de valores
 				wait for 10 ns; -- Retardos
-				Selector <= "0001";
+				Selector_TB <= "0001";
 				wait for 10 ns;
-				Selector <= "0010";
+				Selector_TB <= "0010";
 				wait for 10 ns;
-				Selector <= "0011";
+				Selector_TB <= "0011";
 				wait for 10 ns;
-				Selector <= "0100";
+				Selector_TB <= "0100";
 				wait for 10 ns;
-				Selector <= "0101";
+				Selector_TB <= "0101";
 				wait for 10 ns;
 				wait;
 		end process;-- Fin del proceso de estimulos
