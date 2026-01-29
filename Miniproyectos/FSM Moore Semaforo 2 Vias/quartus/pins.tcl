4b4de2e0c2b24469543607902dda61328f7ef146
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2026-01-28 21:55:40 -0600

    Asignación de pines al FPGA

---
+++ Miniproyectos/FSM Moore Semaforo 2 Vias/quartus/pins.tcl
@@ -0,0 +1,14 @@
+#ASIGNACIÓN DE PINES 
+		
+#ENTRADAS
+set_location_assignment PIN_P11 -to CLK_50MHZ
+set_location_assignment PIN_C10 -to BRST
+
+#SALIDAS
+set_location_assignment PIN_A8 -to S1[0]
+set_location_assignment PIN_A9 -to S1[1]
+set_location_assignment PIN_A10 -to S1[2]
+
+set_location_assignment PIN_D14 -to S2[0]
+set_location_assignment PIN_A11 -to S2[1]
+set_location_assignment PIN_B11 -to S2[2]
