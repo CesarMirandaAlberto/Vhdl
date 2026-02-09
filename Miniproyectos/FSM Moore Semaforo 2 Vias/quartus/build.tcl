6fdc48af88faaa265818de66e8e0a3f3feb78868
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2026-02-08 21:41:29 -0600

    Archivo build

--- Miniproyectos/FSM Moore Semaforo 2 Vias/quartus/build.tcl
+++ Miniproyectos/FSM Moore Semaforo 2 Vias/quartus/build.tcl
@@ -13,11 +13,11 @@ set_global_assignment -name DEVICE 10M50DAF484C7G
 set_global_assignment -name TOP_LEVEL_ENTITY Top
 
 #CONFIGURACION DE ARCHIVOS 
-set_global_assignment -name VHDL_FILE "./Divisor_Frecuencia.vhd"
-set_global_assignment -name VHDL_FILE "./Contador.vhd"
-set_global_assignment -name VHDL_FILE "./FSM_Semaforo2V.vhd"
-set_global_assignment -name VHDL_FILE "./Modulo_Top.vhd"
-set_global_assignment -name VHDL_FILE "./Top.vhd"
+set_global_assignment -name VHDL_FILE "d:/Users/ruta/del/Proyectos/TCLFPGA/VHDL/FSMMELAY/rtl/Divisor_Frecuencia.vhd"
+set_global_assignment -name VHDL_FILE "d:/Users/ruta/del/Proyectos/TCLFPGA/VHDL/FSMMELAY/rtl/Contador.vhd"
+set_global_assignment -name VHDL_FILE "d:/Users/ruta/del/Proyectos/TCLFPGA/VHDL/FSMMELAY/rtl/FSM_Semaforo2V.vhd"
+set_global_assignment -name VHDL_FILE "d:/Users/ruta/del/Proyectos/TCLFPGA/VHDL/FSMMELAY/rtl/Modulo_Top.vhd"
+set_global_assignment -name VHDL_FILE "d:/Users/ruta/del/Proyectos/TCLFPGA/VHDL/FSMMELAY/rtl/Top.vhd"
 
 #quartus_pgm -m jtag -o "p;de10lite_demo.sof"
 #ASIGNAMIENTO DE PINES
