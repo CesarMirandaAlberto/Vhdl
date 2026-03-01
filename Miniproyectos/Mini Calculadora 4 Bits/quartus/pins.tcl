71d0e81dd5ab46898dd3f95f4d41d7413b651cea
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2026-02-28 22:17:09 -0600

    Archivo con los pines físicos del FPGA

---
+++ Miniproyectos/Mini Calculadora 4 Bits/quartus/pins.tcl
@@ -0,0 +1,68 @@
+#ASIGNACIÓN DE PINES 	
+
+# **** ENTRADAS ****
+
+# ENTRADA X
+set_location_assignment PIN_C10 -to X_TOP[0]
+set_location_assignment PIN_C11 -to X_TOP[1]
+set_location_assignment PIN_D12 -to X_TOP[2]
+set_location_assignment PIN_C12 -to X_TOP[3]
+# ENTRADA Y
+set_location_assignment PIN_A13 -to Y_TOP[0]
+set_location_assignment PIN_A14 -to Y_TOP[1]
+set_location_assignment PIN_B14 -to Y_TOP[2]
+set_location_assignment PIN_F15 -to Y_TOP[3]
+
+# SELECTOR 
+set_location_assignment PIN_A12 -to SELECTOR_TOP[0]
+set_location_assignment PIN_B12 -to SELECTOR_TOP[1]
+		
+# **** ENTRADA X ****
+set_location_assignment PIN_J20 -to DISPLAYX[0]
+set_location_assignment PIN_K20 -to DISPLAYX[1] 
+set_location_assignment PIN_L18 -to DISPLAYX[2]
+set_location_assignment PIN_N18 -to DISPLAYX[3]
+set_location_assignment PIN_M20 -to DISPLAYX[4]
+set_location_assignment PIN_N19 -to DISPLAYX[5]
+set_location_assignment PIN_N20 -to DISPLAYX[6]
+
+# **** ENTRADA Y ****
+set_location_assignment PIN_F18 -to DISPLAYY[0]
+set_location_assignment PIN_E20 -to DISPLAYY[1]
+set_location_assignment PIN_E19 -to DISPLAYY[2]
+set_location_assignment PIN_J18 -to DISPLAYY[3]
+set_location_assignment PIN_H19 -to DISPLAYY[4]
+set_location_assignment PIN_F19 -to DISPLAYY[5]
+set_location_assignment PIN_F20 -to DISPLAYY[6]
+
+
+# **** SALIDAS ****
+
+# **** RESULTADO ****
+
+# UNIDADES
+set_location_assignment PIN_C14 -to RESULTADO_UNIDADES[0]
+set_location_assignment PIN_E15 -to RESULTADO_UNIDADES[1]
+set_location_assignment PIN_C15 -to RESULTADO_UNIDADES[2]
+set_location_assignment PIN_C16 -to RESULTADO_UNIDADES[3]
+set_location_assignment PIN_E16 -to RESULTADO_UNIDADES[4]
+set_location_assignment PIN_D17 -to RESULTADO_UNIDADES[5]
+set_location_assignment PIN_C17 -to RESULTADO_UNIDADES[6]
+
+#DECENAS
+set_location_assignment PIN_C18 -to RESULTADO_DECENAS[0]
+set_location_assignment PIN_D18 -to RESULTADO_DECENAS[1]
+set_location_assignment PIN_E18 -to RESULTADO_DECENAS[2]
+set_location_assignment PIN_B16 -to RESULTADO_DECENAS[3]
+set_location_assignment PIN_A17 -to RESULTADO_DECENAS[4]
+set_location_assignment PIN_A18 -to RESULTADO_DECENAS[5]
+set_location_assignment PIN_B17 -to RESULTADO_DECENAS[6]
+
+#CENTENAS
+set_location_assignment PIN_B20 -to RESULTADO_CENTENAS[0]
+set_location_assignment PIN_A20 -to RESULTADO_CENTENAS[1]
+set_location_assignment PIN_B19 -to RESULTADO_CENTENAS[2]
+set_location_assignment PIN_A21 -to RESULTADO_CENTENAS[3]
+set_location_assignment PIN_B21 -to RESULTADO_CENTENAS[4]
+set_location_assignment PIN_C22 -to RESULTADO_CENTENAS[5]
+set_location_assignment PIN_B22 -to RESULTADO_CENTENAS[6]
