eeb7a2ef7d9a2e5a2ed798e7bc96ba7cbcea707b
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2026-03-27 22:39:37 -0600

    Ejecutable para Bajar al FPGA

---
+++ Miniproyectos/FSM Mealy Máquina Expendedora/quartus/BajarAFPGA.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ====== COMPILANDO =====
+quartus_sh -t build.tcl
+
+
+
+echo ====== BAJANDO AL FPGA ========
+quartus_pgm -m jtag -o "p;de10lite_demo.sof"
+
+
+echo ====== CODIGO BAJADO CON EXITO ========
+
+pause
